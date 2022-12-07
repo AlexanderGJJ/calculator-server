0a92bcf78bf457123ff8a48fdbc0b7a5e5f979d2
mikhotin <ad.mihotin@gmail.com>
2022-12-07 23:38:53 +0300

    refresh working

--- src/service/token-service.js
+++ src/service/token-service.js
@@ -3,7 +3,7 @@ const tokenModel = require('../model/tocken-model');
 
 class TokenService {
     generateToken (payload) {
-        const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET, { expiresIn: '30m' });
+        const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET, { expiresIn: '20d' });
         const refreshToken = jwt.sign(payload, process.env.JWT_REFREFSH_SECRET, { expiresIn: '30d' });
         return { accessToken, refreshToken };
     }
